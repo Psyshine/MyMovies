60fde8bef257e0c2de3285d7718cd1bf37fd7243
Andry Pro <isp.fline@gmail.com>
2020-03-25 14:37:30 +0200

    Initial Commit

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Andry Pro on 25.03.2020.
+//  Copyright © 2020 Andry Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
